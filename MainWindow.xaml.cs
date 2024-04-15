e0bd8317c7a752dc44d6f34accf070ea97d12fd1
domik <domik@LAPTOP-64VC4KM7>
2024-04-15 19:59:22 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Prakt10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prakt13_Rodin_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int[,] matr;
+
+        private void CreateMatric_Clicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int str = (int)kolRow.Value;
+                int column = (int)kolColumn.Value;
+                matr = new int[str, column];
+                datagrib.ItemsSource = VisualArray.ToDataTable(matr).DefaultView;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void FillMatric_Clicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int min = (int)minRand.Value;
+                int max = (int)maxRand.Value;
+                Random rnd = new Random();
+
+                for (int i = 0; i < matr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matr.GetLength(1); j++)
+                    {
+                        matr[i, j] = rnd.Next(min, max);
+                    }
+                }
+
+                datagrib.ItemsSource = VisualArray.ToDataTable(matr).DefaultView;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ClearMatric_Clicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                matr = null;
+                datagrib.ItemsSource = null;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void GetResult_Clicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int maxOfAll = 0;
+                int polozh = 0;
+                int strMax = 0;
+
+                for (int i = 0; i < matr.GetLength(0); i++)
+                {
+                    polozh = 0;
+                    for (int j = 0; j < matr.GetLength(1); j++)
+                    {
+                        if (matr[i, j] > 0) polozh++;
+                    }
+                    if (polozh > maxOfAll)
+                    {
+                        maxOfAll = polozh;
+                        strMax = i;
+                    }
+                }
+                strMax++;
+                textResults.Text = strMax.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Info_Clicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Калитин С.А. ИСП-41\nИмеется матрица размера N*M. Определить в какой строке количество положительных элементов наибольшее");
+        }
+    }
+}
---
+++ Pract5Funcs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract5
+{
+    internal class Pract5Funcs
+    {
+        /// <summary>
+        /// Заполняет двумерный целочисленный массив случайными значениями
+        /// </summary>
+        /// <param name="arr">Двумерный целочисленный массив</param>
+        /// <param name="min">Минимальное значение</param>
+        /// <param name="max">Максимальное значение</param>
+        public static void FillArr(ref int[,] arr, int min, int max)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+                for (int j = 0; j < arr.GetLength(1); j++) 
+                    arr[i, j] = rnd.Next(min, max);
+        }
+
+        /// <summary>
+        /// Возвращает номера столбцов, произведение отрицательных элементов которых является положительным числом
+        /// </summary>
+        /// <param name="arr">Двумерный целочисленный массив</param>
+        /// <returns>Коллекция номеров столбцов</returns>
+        public static List<int> GetNegativesMult(int[,] arr)
+        {
+            List<int> cols = new List<int>();
+            int count;
+
+            for (int i = 0; i < arr.GetLength(1); i++)
+            {
+                count = 0;
+                for (int j = 0; j < arr.GetLength(0); j++)
+                    if (arr[j, i] < 0) count++;
+                
+                if (count > 0 && count % 2 == 0) cols.Add(i);
+            }
+
+            return cols;
+        }
+    }
+}
